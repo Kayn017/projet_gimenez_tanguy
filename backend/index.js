17748f1145c805149c56506332901815638e875a
Tanguy Gimenez <gimeneztanguy@gmail.com>
2024-04-19 22:02:07 +0200

    backend

---
+++ backend/index.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+
+const Fastify = require("fastify");
+const products = require("./products.json");
+
+const jwt = require("@fastify/jwt");
+
+const tokenSecret = process.env.TOKEN_SECRET;
+
+if(!tokenSecret) {
+    console.error("TOKEN_SECRET is required");
+    process.exit(1);
+}
+
+const server = Fastify({ logger: true });
+
+server.register(jwt, { secret: tokenSecret });
+
+server.get("/", async (request, reply) => {    
+    return { hello: "world" };
+});
+
+server.get("/protected", async (request, reply) => {
+    try {
+        await request.jwtVerify();
+        return { protected: "data" };
+    } catch (err) {
+        reply.code(401).send({ message: "Unauthorized" });
+    }
+});
+
+server.post("/login", async (request, reply) => {
+    const { username, password } = request.body;
+
+    // Simulate a login
+    if(username === "admin" && password === "admin") {
+        const token = server.jwt.sign({ username });
+        return { token };
+    }
+
+    reply.code(401).send({ message: "Unauthorized" });
+});
+
+server.get("/products", async (request, reply) => {
+    return products;
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen({ port }, (err, address) => {
+    if (err) 
+        throw err;
+
+    server.log.info(`server listening on ${address}`);
+});
